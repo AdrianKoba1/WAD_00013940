257c50d67b57f9b0e0b6af533e45de1055f21f03
00013940 <bestkomilov2003@gmail.com>
2024-03-11 13:20:01 +0500

    Initial Commit

---
+++ Data/TaskTrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _00013940_TaskTracker.Data
+    
+{
+    public class TaskTrackerDbContext : DbContext
+    {
+        public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
